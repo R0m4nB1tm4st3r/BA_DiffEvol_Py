756f435008882f4f588003f4431b04183c8d8cde
Roman Hochhalter <roman.hochhalter@evopro-ag.de>
2019-10-04 10:02:32 +0200

    initial commit

---
+++ DifferentialEvolution/diff_evol.py
@@ -0,0 +1,109 @@
+#############################################################################################################################
+#####################################--Imports--#############################################################################
+#############################################################################################################################
+import numpy as np
+import random as rand
+import time
+
+from itertools import repeat
+#############################################################################################################################
+#####################################--Class Declarations--##################################################################
+#############################################################################################################################
+class DE_Handler(object):
+    """ Handler class holding the necessary predefined parameters for Differential Evolution
+    and implementing the DE algorithm
+
+    properties:
+    - F:                    mutation scale factor, positive real number typically less than 1
+    - Cr:                   crossover constant, positive real number between 0 and 1
+    - G:                    maximum number of generations/iterations for DE algorithm
+    - Np:                   population size
+    - population:           initial population the algorithm is executed on
+    - ObjectiveFunction:    the function whose parameters need to be optimized by the algorithm
+    - minimizeFlag:         specifies whether it is an Minimization or an Maximization problem
+    - minBounds:            minimum values for parameters to find
+    - maxBounds:            maximum values for parameters to find
+    """
+
+    def __init__(self, F, Cr, G, Np, population, ObjectiveFunction, minimizeFlag, minBounds, maxBounds):
+        self.F = F
+        self.Cr = Cr
+        self.G = G
+        self.Np = Np
+        self.population = population
+        self.ObjectiveFunction = ObjectiveFunction
+        self.minimizeFlag = minimizeFlag
+        self.minBounds = minBounds
+        self.maxBounds = maxBounds
+#############################################################################################################################
+    def EvaluatePopulation(self):
+        """ Finds the member of the current population that yields the best value for the Objective Function
+        (doesn't need to be called externally)
+        """
+        values = np.array([self.ObjectiveFunction(self.minBounds + self.population[p] * (self.maxBounds - self.minBounds)) for p in range(self.Np)])
+
+        if self.minimizeFlag == True:
+            best = (self.population[np.argmin(values)], np.amin(values))
+        else:
+            best = (self.population[np.argmax(values)], np.amax(values))
+
+        return best, values
+#############################################################################################################################
+    def GetMutantVector(self, bestParams, memberIndex):
+        """ Calculates the Mutant Vectors for the current population
+        (doesn't need to be called externally)
+
+        - bestParams: param vector of the best member in current population
+        - memberIndex: index of the currently evaluated member of the present population
+        """
+
+        randChoice = np.random.randint(0, self.Np-1, 6)
+        randNumbers = np.random.choice(randChoice[randChoice != memberIndex], 2)
+
+        v = (bestParams + (self.population[randNumbers[0]] - self.population[randNumbers[1]]) * self.F)
+        v = np.select([v < 0, v > 1], [rand.random(), rand.random()], v)
+
+        return v
+#############################################################################################################################
+    def SelectVector(self, v, p, value):
+        """ Crosses the population with the Mutant Vectors and creates the Trial Vectors
+        (doesn't need to be called externally)
+
+        - v: the mutant vector of the currently evaluated member
+        - p: the currently evaluated member
+        - value: return value of the objective function for the current member
+        """
+
+        r = rand.randint(0, v.size-1)
+        L = 1
+        while rand.random() <= self.Cr and L < v.size:
+            L = L + 1
+
+        u = np.array([v[(r+i)%v.size] if i < L else p[(r+i)%v.size] for i in range(v.size)])
+
+        valueU = self.ObjectiveFunction(self.minBounds + u * (self.maxBounds - self.minBounds))
+
+        if (valueU <= value and self.minimizeFlag == True) or (valueU >= value and self.minimizeFlag == False):
+            return u
+        else:
+            return p
+#############################################################################################################################
+    def DE_Optimization(self):
+        """ executes the whole optimization process of the DE-Algorithm
+        (doesn't need to be called externally)
+        """
+
+        best, currentValues = self.EvaluatePopulation()
+        self.population = [self.SelectVector(self.GetMutantVector(best[0], j), self.population[j], currentValues[j]) for j in range(self.Np)]
+        return best[1]
+#############################################################################################################################
+    def DE_GetBestParameters(self):
+        """ starts the optimization process and then returns the last found best parameters
+        """
+        t0 = time.time()
+        bestValueHistory = np.array([self.DE_Optimization() for _ in range(self.G)])
+        print(time.time()-t0)
+        best, currentValues = self.EvaluatePopulation()
+        return best, bestValueHistory
+
+
---
+++ DifferentialEvolution/seg.py
@@ -0,0 +1,313 @@
+#############################################################################################################################
+#####################################--Imports--#############################################################################
+#############################################################################################################################
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+import cv2
+import math
+import random as rand
+import time
+import diff_evol as de
+import os
+
+from tesserocr import PyTessBaseAPI, RIL
+from PIL import Image
+from itertools import repeat
+from scipy.stats import norm
+from scipy.ndimage import gaussian_filter
+#############################################################################################################################
+#####################################--Functions--###########################################################################
+#############################################################################################################################
+def GetImage(path, mode):
+    """
+    read out an image from given file path
+    """
+    return cv2.imread(path, mode) # cv2.IMREAD_GRAYSCALE)
+#############################################################################################################################
+def ShowImage(image):
+    """
+    display image in separate window that waits for any key input to close
+    """
+    cv2.imshow('', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+#############################################################################################################################
+def SaveImage(image, path):
+    cv2.imwrite(path, image)
+#############################################################################################################################
+def CalculateRawHistogram(image):
+    """
+    calculate histogram for image that displays the absolute numbers of gray levels
+
+    - image: input image to calculate the histogram for
+    """
+    h = np.zeros(256, np.float_)
+    for i in np.nditer(image):
+        h[i - 1] = h[i - 1] + 1
+
+    return h
+#############################################################################################################################
+def CalculateNormalizedHistogram(image):
+    """
+    calculate histogram for image that displays the relative numbers of gray levels
+
+    - image: input image to calculate the histogram for
+    """
+
+    raw = CalculateRawHistogram(image)
+    norm = np.zeros(256, np.float_)
+
+    for i in range(256):
+        norm[i] = raw[i] / image.size
+
+    return norm
+#############################################################################################################################
+def Plot_SingleGraph(dataHorizontal, dataVertical, horizontalLabel, verticalLabel, title, graphLabel):
+    """
+    create a figure with a single graph containing the given data
+
+    - dataHorizontal: data for the horizontal axis of the graph
+    - dataVertical: data for the vertical axis of the graph
+    - horizontalLabel: label for the horizontal axis
+    - verticalLabel: label for the vertical axes
+    - title: title of the graph
+    - graphLabel: legend entry for the graph
+    """
+
+    diagram, ax = plt.subplots()
+    ax.plot(dataHorizontal, dataVertical, label=graphLabel)
+
+    plt.xlabel(horizontalLabel)
+    plt.ylabel(verticalLabel)
+    plt.title(title)
+#############################################################################################################################
+def PlotInSameFigure(dataHorizontal, dataVerticalArray, graphLabelArray):
+    """
+    plot several graphs in one figure
+
+    - dataHorizontal: data for the horizontal axis of the graph
+    - dataVerticalArray: data for more than one graph for the vertical axis
+    - graphLabelArray: legend entries for each graph
+    """
+
+    for i in range(len(dataVerticalArray)):
+        plt.plot(dataHorizontal, dataVerticalArray[i], label=graphLabelArray[i])
+
+#############################################################################################################################
+def PlotWithSubPlots(dataHorizontal, dataVerticalArray, graphLabelArray, yDim, xDim):
+    """
+    plot several graphs in one figure creating subplots for each graph
+
+    - dataHorizontal: data for the horizontal axis of the graph
+    - dataVerticalArray: data for more than one graph for the vertical axis
+    - graphLabelArray: legend entries for each graph
+    - yDim: number of lines
+    - xDim: number of columns
+    """
+
+    fig, ax_list = plt.subplots(xDim, yDim)
+    for i in range(len(dataVerticalArray)):
+        ax_list[i].plot(dataHorizontal, dataVerticalArray[i])
+        ax_list[i].legend(( graphLabelArray[i], ))
+#############################################################################################################################
+def CreateSubplotGrid(rows, columns, shareX):
+    """
+    creates a subplot grid object 
+
+    - rows: number of rows
+    - columns: number of columns
+    - shareX: determines whether all subplots share the x-axis or not
+    """
+    fig, ax = plt.subplots(rows, columns, shareX)
+    return fig, ax
+#############################################################################################################################
+def SumOfGauss(param_list, classNum, g_lvls):
+    """
+    calculate histogram approximation as sum of gaussian probability density distribution functions
+
+    - param_list: list of gaussian parameters (P1, P2, ... Pk, my1, my2, ... myk, sigma1, sigma2, ... sigmak - k is the number of classes) 
+    - classNum: number of classes
+    - g_lvls: list of graylevels
+    """
+    return sum([param_list[i] * norm.pdf(g_lvls, loc=param_list[i + classNum], scale=param_list[i + classNum * 2]) for i in range(classNum)])
+#############################################################################################################################
+def CalcErrorEstimation(param_list):
+    """
+    calculate mean sqare error between histogram and gaussian approximation including penalty in case sum(Pi) != 1
+
+    - param_list: list of gaussian parameters (P1, P2, ... Pk, my1, my2, ... myk, sigma1, sigma2, ... sigmak - k is the number of classes) 
+    """
+    classNum = K
+    g_lvls = graylevels
+    histogram = h 
+    o = 1.5
+
+    result = sum(( SumOfGauss(param_list, classNum, g_lvls) - histogram ) ** 2) / g_lvls.size + abs(sum(param_list[:classNum]) - 1) * o
+    return result
+#############################################################################################################################
+def TestFunction_SimpleParabolic(x):
+    """
+    calculate the square of input x
+
+    - x: value for the function to be evaluated
+    """
+    return x ** 2
+#############################################################################################################################
+def InitializePopulation(Np, paramNum):
+    """
+    initialize population with shape given by input parameters
+
+    - Np: 
+    - paramNum: 
+    """
+    population = np.random.rand(Np, paramNum)
+    return population
+#############################################################################################################################
+def CalculateThresholdValues(param_list, classNum):
+    """
+    calculate threshold values for image segmentation
+
+    - param_list: list of gaussian parameters (P1, P2, ... Pk, my1, my2, ... myk, sigma1, sigma2, ... sigmak - k is the number of classes) 
+    - classNum: number of classes
+    """
+    thresholdValues = np.arange(classNum - 1, dtype=np.uint8)
+    indexOrder = np.argsort(param_list[classNum:classNum * 2])
+
+    P = [param_list[indexOrder[i]] for i in range(classNum)]
+    my = np.sort(param_list[classNum:classNum * 2])
+    sigma = [param_list[classNum * 2 + indexOrder[i]] for i in range(classNum)]
+
+    for i in range(classNum - 1):
+        a = sigma[i] ** 2 - sigma[i + 1] ** 2
+        b = 2 * ( my[i] * ( sigma[i + 1] ** 2 ) - my[i + 1] * ( sigma[i] ** 2 ) )
+        c = ( sigma[i] * my[i + 1] ) ** 2 - ( sigma[i + 1] * my[i] ) ** 2 + 2 * ( ( sigma[i] * sigma[i + 1] ) ** 2 ) * math.log(( ( sigma[i + 1] * P[i] ) / ( sigma[i] * P[i + 1] ) ))
+
+        p = np.poly1d([a, b, c], False, "T")
+        p_roots = np.roots(p)
+        if p_roots.size == 1:
+            thresholdValues[i] = p_roots[0]
+        elif p_roots[0] == p_roots[1]:
+            thresholdValues[i] = p_roots[0]
+        elif p_roots[0] < 0:
+            thresholdValues[i] = p_roots[1]
+        elif p_roots[1] < 0:
+            thresholdValues[i] = p_roots[0]
+        elif p_roots[0] > 255:
+            thresholdValues[i] = p_roots[1]
+        elif p_roots[1] > 255:
+            thresholdValues[i] = p_roots[0]
+        else:
+            if i > 0:
+                if not(np.amin(p_roots) <= thresholdValues[i-1]):
+                    thresholdValues[i] = np.amin(p_roots)
+                else:
+                    thresholdValues[i] = np.amax(p_roots)
+            else:
+                if np.amin(p_roots) <= my[i]:
+                    thresholdValues[i] = np.amax(p_roots)
+                else:
+                    thresholdValues[i] = np.amin(p_roots)
+
+    return thresholdValues
+#############################################################################################################################
+def DoImageSegmentation(image, thresholdValues):
+    """
+    examine segmentation on input image based on input threshold values
+
+    - image: image to be segmentated
+    - thresholdValues: limit graylevels defining the classes
+    """
+    newImage = np.copy(image)
+    values = np.arange(1, dtype=np.uint8)
+    values = np.append(values, thresholdValues[0:thresholdValues.size - 1])
+    condList = [newImage < thresholdValues[i] for i in range(thresholdValues.size)]
+
+    newImage = np.select(condList, values, thresholdValues[thresholdValues.size - 1])
+    return newImage
+#############################################################################################################################
+#####################################--Globals--#############################################################################
+#############################################################################################################################
+K = 5
+G = 250
+img1 = GetImage('Y:\MobileOcr\Bilder\TestSet_DivBauteile\All_Images\good\IMG_1255_DLXFFNICKLT_c.jpg', cv2.IMREAD_GRAYSCALE)
+#tesseract_testImage = Image.open("opencv-logo2.png")
+#img1 = cv2.fastNlMeansDenoising(img1, None, 5, 5, 77)
+#img1 = cv2.medianBlur(img1, 3)
+#img1 = gaussian_filter(img1, sigma=0.73, order=2)
+ShowImage(img1)
+h = CalculateNormalizedHistogram(img1)
+graylevels = np.arange(256)
+#############################################################################################################################
+#######################################--Main--##############################################################################
+#############################################################################################################################
+if __name__ == "__main__":
+    print('Hello World')
+
+    t_min = np.array([])
+    t_max = np.array([])
+    t_min = np.append(t_min, [list(repeat(0., K)), list(repeat(0., K)), list(repeat(0., K))]) 
+    t_max = np.append(t_max, [list(repeat(1., K)), list(repeat(255., K)), list(repeat(12., K))]) 
+
+    print("Press Enter to start")
+
+    while input() != "n":
+
+        print("Optimizing...")
+        np.random.seed(123)
+        plotFigure, plotAxes = CreateSubplotGrid(2, 1, False)
+
+        test_population = InitializePopulation(3*10*K, 3*K)
+        de_handle = de.DE_Handler(0.3, 0.70, G, 3*10*K, test_population, CalcErrorEstimation, True, t_min, t_max)
+
+        bestParams, bestValueHistory = de_handle.DE_GetBestParameters()
+        bestMember = t_min + bestParams[0] * ( t_max - t_min )
+        thresholdValues = CalculateThresholdValues(bestMember, K)
+
+        newImage = DoImageSegmentation(img1, thresholdValues)
+
+        plotFigure.suptitle("Results of Differential Evolution Algorithm with " + str(G) + " iterations", fontsize=14)
+        plotFigure.set_dpi(200)
+
+        currentTime = time.strftime("%H:%M:%S").replace(":", "_")
+        currentDate = time.strftime("%d/%m/%Y").replace("/", "_")
+        fileName = "IMG_1255_DLXFFNICKLT_c-" + "SEG_Test-" + currentDate + "-" + currentTime + "-" + str(G) + "-" + str(K) + ".jpg"
+
+        plotAxes[0].plot(graylevels, h)
+        plotAxes[0].plot(graylevels, SumOfGauss(bestMember, K, graylevels))
+        plotAxes[0].legend(("Histogram of the original Image", "Gaussian Approximation of the Histogram", ))
+        plotAxes[0].vlines(thresholdValues, 0, np.amax(h), label="Threshold Values")
+        for i in range(thresholdValues.size):
+            plotAxes[0].annotate("T" + str(i+1), xy=(thresholdValues[i], np.amax(h)), )
+        plotAxes[0].set_xlabel("Graylevel g")
+        plotAxes[0].set_ylabel("n_Pixel_relative")
+        plotAxes[0].set_title("Mean Square Error: " + str(bestParams[1]))
+        plotAxes[1] = plt.imshow(newImage, cmap='gray', vmin=0, vmax=255)
+        plotAxes[1].axes.set_title("Result of Image Segmentation")
+        plt.tight_layout()
+        plt.savefig("IMG_1255_DLXFFNICKLT_c-" + "SEG_Plot-" + currentDate + "-" + currentTime + "-" + str(G) + "-" + str(K) + ".png", format="png", dpi=200)
+        plt.show()
+
+        SaveImage(newImage, fileName)
+        seg_image = Image.open(fileName)
+
+        with PyTessBaseAPI() as api:
+            api.SetImage(seg_image)
+            boxes = api.GetComponentImages(RIL.TEXTLINE, True)
+
+            print("Found {} textline image components.".format(len(boxes)))
+            for i, (im, box, _, _) in enumerate(boxes):
+                api.SetRectangle(box["x"], box["y"], box["w"], box["h"])
+                ocrResult = api.GetUTF8Text()
+                conf = api.MeanTextConf()
+                print(u"Box[{0}]: x={x}, y={y}, w={w}, h={h}, "
+                  "confidence: {1}, text: {2}".format(i, conf, ocrResult, **box))
+
+        print("Do you wish to make another plot? (y/n)")
+
+#top=0.902,
+#bottom=0.026,
+#left=0.065,
+#right=0.978,
+#hspace=0.3,
+#wspace=0.2
