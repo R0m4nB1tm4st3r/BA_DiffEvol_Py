8f21f0b7cec10e4950ab19b28206e87fb0e10e90
Roman Hochhalter <roman.hochhalter@evopro-ag.de>
2019-11-22 13:58:28 +0100

    try all permutations of threshold values in OF1

--- DifferentialEvolution/main.py
+++ DifferentialEvolution/main.py
@@ -13,10 +13,9 @@ import objFuncs as obf
 import plot
 import seg
 
-from tesserocr import PyTessBaseAPI, RIL
 from PIL import Image
 from itertools import repeat
-from scipy.stats import norm
+from itertools import product
 
 #############################################################################################################################
 #####################################--Functions--###########################################################################
@@ -46,13 +45,26 @@ if __name__ == "__main__":
     Cr = 0.9
     o = 1.5
     K = 5
-    G = 20
+    G = 5000
+    populationSize_OF2 = 5*K
+    numOfImgs = 1
+    #imageID = 3
 
     print("Put in the Test Number to start with:")
     testNumber = int(input())
-    imgStrings = np.array(["IMG_1255_DLXFFNICKLT_c", "IMG_1324_DLXFFNICKLT_c", "IMG_1349_0ZTTM0TUN3H_c", "IMG_1555_7D91CX6GNPA_c"])
-    images = np.array(list((seg.GetImage(imgStrings[i] + ".jpg", cv2.IMREAD_GRAYSCALE) for i in range(4))))
-    #images = np.array(list((cv2.fastNlMeansDenoising(GetImage(imgStrings[i] + ".jpg", cv2.IMREAD_GRAYSCALE), None, 10, 7, 21) for i in range(4))))
+    imgStrings = np.array(["IMG_01002DOKR5B_c", "IMG_1255_DLXFFNICKLT_c", "IMG_1324_DLXFFNICKLT_c", "IMG_1349_0ZTTM0TUN3H_c", "IMG_1555_7D91CX6GNPA_c"])
+    images = np.array(list((seg.GetImage(imgStrings[i] + ".jpg", cv2.IMREAD_GRAYSCALE) for i in range(numOfImgs))))
+    #images = np.array(list((cv2.fastNlMeansDenoising(seg.GetImage(imgStrings[i] + ".jpg", cv2.IMREAD_GRAYSCALE), None, 20, 7, 21) for i in range(numOfImgs))))
+
+    #laplacian = cv2.Laplacian(images[0],cv2.CV_8U)
+    #sobelx = cv2.Sobel(images[0],cv2.CV_8U,2,0,ksize=3)
+    #sobely = cv2.Sobel(images[0],cv2.CV_8U,0,2,ksize=3)
+    #sobelxy = cv2.Sobel(images[0],cv2.CV_8U,4,4,ksize=5)
+
+    #seg.ShowImage(laplacian)
+    #seg.ShowImage(sobelx)
+    #seg.ShowImage(sobely)
+    #seg.ShowImage(sobelxy)
 
     graylevels = np.arange(256)
 
@@ -82,29 +94,41 @@ if __name__ == "__main__":
 
     de_param_string = "G_" + str(G) + "-" + "K_" + str(K) + "-" + "F_" + str(F) + "-" + "Cr_" + str(Cr)
 
-    for j in range(4):
+    for j in range(numOfImgs):
         print("Optimizing...")
         np.random.seed(123)
 
         # Initialize needed parameters for DE #
         h = obf.OF1_CalculateNormalizedHistogram(images[j])
-        #test_population = InitializePopulation(3*10*K, 3*K)
-        #objArgs = (K, graylevels, h, o)
-        testPopulation_OF2 = InitializePopulation(3*K, K)
-        objArgs_OF2 = (images[j],)
+        
+        test_population = InitializePopulation(3*10*K, 3*K)
+        objArgs = (K, graylevels, h, o)
+        #testPopulation_OF2 = InitializePopulation(populationSize, K)
+        #objArgs_OF2 = (images[j],)
 
         # Execute DE and do Segmentation of the current image #
-        #de_handle = de.DE_Handler(F, Cr, G, 3*10*K, test_population, obf.OF1_CalcErrorEstimation, True, t_min, t_max, objArgs)
-        de_OF2 = de.DE_Handler(F, Cr, G, 3*K, testPopulation_OF2, obf.OF2_Calc_InterClusterDistance, True, minBounds_OF2, maxBounds_OF2, objArgs_OF2)
-        #bestParams, bestValueHistory = de_handle.DE_GetBestParameters()
-        #bestMember = bestParams[0]
-        #thresholdValues = obf.OF1_CalculateThresholdValues(bestMember, K)
+        de_handle = de.DE_Handler(F, Cr, G, 3*10*K, test_population, obf.OF1_CalcErrorEstimation, True, t_min, t_max, objArgs)
+        #de_OF2 = de.DE_Handler(F, Cr, G, populationSize, testPopulation_OF2, obf.OF2_Calc_InterClusterDistance, True, minBounds_OF2, maxBounds_OF2, objArgs_OF2)
+        
+        bestParams, bestValueHistory = de_handle.DE_GetBestParameters()
+        bestMember = bestParams[0]
+        thresholdValues = obf.OF1_CalculateThresholdValues(bestMember, K)
+        thresholdCombinations = np.array(list(product(*thresholdValues)))
         #newImage = obf.OF1_DoImageSegmentation(images[j], thresholdValues, K, rgbColorList)
-        bestMember, sumHistory = de_OF2.DE_GetBestParameters()
-        centers = np.sort(bestMember[0])
-        segments = obf.OF2_GetSegments(centers, images[j])
-        newImage = obf.OF2_DoImageSegmentation(segments, images[j], rgbColorList)
-        seg.ShowImage(newImage)
+        newImages = np.array([obf.OF1_DoImageSegmentation(images[j], thresholdCombinations[t], K, rgbColorList) for t in range(thresholdCombinations.shape[0]) \
+            if np.amin(thresholdCombinations[t]) != -1])
+        for n in range(newImages.shape[0]):
+            newImages[n] = cv2.cvtColor(newImages[n], cv2.COLOR_RGB2BGR)
+            seg.ShowImage(newImages[n])
+            seg.SaveImage(newImages[n], "segTest_" + str(n) + ".jpg")
+            seg_image = Image.open("segTest_" + str(n) + ".jpg")
+            seg.Tesseract_ReadTextFromImage(seg_image)
+        #bestMember, sumHistory = de_OF2.DE_GetBestParameters()
+        #centers = np.sort(bestMember[0])
+        #segments = obf.OF2_GetSegments(centers, images[j])
+        #newImage = obf.OF2_DoImageSegmentation(segments, images[j], rgbColorList)
+        #seg.ShowImage(newImage)
+
         currentTime = time.strftime("%H:%M:%S").replace(":", "_")
         timeString = currentDate + "-" + currentTime
         segImgFileName = imgStrings[j] + "-" + "SEG_Test-" + timeString + "-" + de_param_string + ".jpg" #+ "-" + "No_" + str(x) + ".jpg"
@@ -112,10 +136,10 @@ if __name__ == "__main__":
 
         # plot the Fitness values #
         valHistFigure, valHistAxes = plot.CreateSubplotGrid(1, 1, False)
-        valHistAxes.plot(range(1, G+1), sumHistory)
+        valHistAxes.plot(range(1, G+1), bestValueHistory)
         valHistAxes.set_xlabel("Iteration Number")
-        valHistAxes.set_ylabel("Distance Sum between Clusters")
-        valHistAxes.set_title("Cluster Distance Sum through iterations of DE")# run " + str(x))
+        valHistAxes.set_ylabel("Mean Square Error")
+        valHistAxes.set_title("Mean Square Error History through iterations of DE")# run " + str(x))
         plt.savefig(imgStrings[j] + "-" + "objFuncHist-" + timeString + "-" + de_param_string + ".jpg", dpi=200) #"-" + "No_" + str(x) + ".jpg", dpi=200)
         #plt.show(block=False)
 
@@ -123,18 +147,18 @@ if __name__ == "__main__":
         plotFigure, plotAxes = plot.CreateSubplotGrid(2, 1, False)
         plotFigure.set_dpi(200)
         plotAxes[0].plot(graylevels, h)
-        #plotAxes[0].plot(graylevels, obf.OF1_SumOfGauss(bestMember, K, graylevels))
-        #plotAxes[0].legend(("Histogram of the original Image", "Gaussian Approximation of the Histogram", ))
-        plotAxes[0].legend(("Histogram of the original Image", ))
-        #plotAxes[0].vlines(thresholdValues, 0, np.amax(h), label="Threshold Values")
-        plotAxes[0].vlines(centers[0], 0, np.amax(centers), label="Cluster Centers")
-        #for k in range(thresholdValues.size):
-            #plotAxes[0].annotate("T" + str(k+1), xy=(thresholdValues[k], np.amax(h)), )
-        for k in range(K):
-            plotAxes[0].annotate("c" + str(k+1), xy=(centers[k], np.amax(centers)), )
+        plotAxes[0].plot(graylevels, obf.OF1_SumOfGauss(bestMember, K, graylevels))
+        plotAxes[0].legend(("Histogram of the original Image", "Gaussian Approximation of the Histogram", ))
+        #plotAxes[0].legend(("Histogram of the original Image", ))
+        plotAxes[0].vlines(thresholdValues, 0, np.amax(h), label="Threshold Values")
+        #plotAxes[0].vlines(centers[0], 0, np.amax(h), label="Cluster Centers")
+        for k in range(thresholdValues.size):
+            plotAxes[0].annotate("T" + str(k+1), xy=(thresholdValues[k], np.amax(h)), )
+        #for k in range(K):
+            #plotAxes[0].annotate("c" + str(k+1), xy=(centers[k], np.amax(centers)), )
         plotAxes[0].set_xlabel("Graylevel g")
         plotAxes[0].set_ylabel("n_Pixel_relative")
-        #plotAxes[0].set_title("Mean Square Error: " + str(bestParams[1]))
+        plotAxes[0].set_title("Mean Square Error: " + str(bestParams[1]))
         plotAxes[1] = plt.imshow(newImage, cmap='gray')
         plotAxes[1].axes.set_title("Result of Image Segmentation")
         plt.tight_layout()
@@ -150,4 +174,4 @@ if __name__ == "__main__":
         ocrEndResult = seg.Tesseract_ReadTextFromImage(seg_image)
 
         # write the results into csv file #
-        csv_writer.writerow([j+testNumber, imgStrings[j], currentTime, K, G, F, Cr, ocrEndResult])
+        csv_writer.writerow([j+testNumber, imgStrings[j], currentTime.replace("_", ":"), K, G, str(F).replace(".", ","), str(Cr).replace(".", ","), ocrEndResult])
--- DifferentialEvolution/objFuncs.py
+++ DifferentialEvolution/objFuncs.py
@@ -62,7 +62,7 @@ def OF1_CalculateThresholdValues(param_list, classNum):
     - param_list: list of gaussian parameters (P1, P2, ... Pk, my1, my2, ... myk, sigma1, sigma2, ... sigmak - k is the number of classes) 
     - classNum: number of classes
     """
-    thresholdValues = np.arange(classNum - 1, dtype=np.uint8)
+    thresholdValues = [(-1., -1.) for _ in range(classNum-1)] # np.arange(classNum - 1)
     #numRow = sp.math.factorial(classNum-1)
     #numCol = classNum-1
     #thresholdValues = np.arange(numCol*numRow).reshape(numRow, numCol)
@@ -79,35 +79,30 @@ def OF1_CalculateThresholdValues(param_list, classNum):
 
         p = np.poly1d([a, b, c], False, "T")
         p_roots = np.roots(p)
-        p.dtype = np.uint8
-        r1 = np.real(p_roots[0])
+
         if p_roots.size == 1:
-            thresholdValues[i] = r1
+            thresholdValues[i] = np.real(p_roots[0])
         else:
+            r1 = np.real(p_roots[0])
             r2 = np.real(p_roots[1])
-            if r1 == r2:
-                thresholdValues[i] = r1
-            elif r1 < 0:
-                thresholdValues[i] = r2
-            elif r2 < 0:
-                thresholdValues[i] = r1
-            elif r1 > 255:
-                thresholdValues[i] = r2
-            elif r2 > 255:
-                thresholdValues[i] = r1
+            if (r1 == r2) or (r2 < 0.) or (r2 > 255.):
+                thresholdValues[i] = (r1, -1)
+            elif (r1 < 0) or (r1 > 255):
+                thresholdValues[i] = (r2, -1)
             else:
-                r1 = np.amin(p_roots)
-                r2 = np.amax(p_roots)
-                if i > 0:
-                    if r1 >= thresholdValues[i-1]:
-                        thresholdValues[i] = r1
-                    else:
-                        thresholdValues[i] = r2
-                else:
-                    if (r1 >= my[i]) and (r1 < my[i+1]):
-                        thresholdValues[i] = r1
-                    else:
-                        thresholdValues[i] = r2
+                thresholdValues[i] = (r1, r2)
+                #r1 = np.amin(p_roots)
+                #r2 = np.amax(p_roots)
+                #if i > 0:
+                    #if r1 >= thresholdValues[i-1]:
+                        #thresholdValues[i] = r1
+                    #else:
+                        #thresholdValues[i] = r2
+                #else:
+                    #if (r1 >= my[i]) and (r1 < my[i+1]):
+                        #thresholdValues[i] = r1
+                    #else:
+                        #thresholdValues[i] = r2
 
     return thresholdValues
 #############################################################################################################################
@@ -119,24 +114,26 @@ def OF1_DoImageSegmentation(image, thresholdValues, K, rgbColorList):
     - thresholdValues: limit graylevels defining the classes
     """
     color_image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
-    #newImage = np.copy(image)
-    #values = np.arange(1, dtype=np.uint8)
-    #values = np.arange(3*(K-1), dtype=np.uint8).reshape(K-1, 3)
-    #values[0:K-1] = rgbColorList[0:K-1]
-    #values = np.append(values, thresholdValues[0:thresholdValues.size - 1])
-    #condList = [image < thresholdValues[i] for i in range(thresholdValues.size)]
+    it = np.nditer(image, flags=["multi_index"])
 
-    #newImage = np.select(condList, values, thresholdValues[thresholdValues.size - 1])
-    #color_image = np.select(condList, values, rgbColorList[(rgbColorList.shape[0]) - 1])
-    for i in range(image.shape[0]):
-        for j in range(image.shape[1]):
-            for k in range(K-2, -1, -1):
-                if image[i, j] > thresholdValues[k]:
-                    color_image[i, j] = rgbColorList[k+1]
-                    break
-                else:
-                    color_image[i, j] = rgbColorList[0]
-    #color_image = np.array([], dtype=np.uint8)
+    while not it.finished:
+        for t in range(thresholdValues.size-1, -1, -1):
+            if it[0] > thresholdValues[t]:
+                color_image[it.multi_index] = rgbColorList[t+1]
+                break
+            else:
+                color_image[it.multi_index] = rgbColorList[0]
+        it.iternext()
+
+    #for i in range(image.shape[0]):
+        #for j in range(image.shape[1]):
+            #for k in range(K-2, -1, -1):
+                #if image[i, j] > thresholdValues[k]:
+                    #color_image[i, j] = rgbColorList[k+1]
+                    #break
+                #else:
+                    #color_image[i, j] = rgbColorList[0]
+
     return color_image
 #############################################################################################################################
 def OF2_Calc_InterClusterDistance(centers, inputData):
